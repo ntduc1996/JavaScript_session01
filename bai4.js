9daf2821f3633f05e1312d5fbcfc89ef29dedaa1
ntduc1996 <137298627+ntduc1996@users.noreply.github.com>
2025-04-21 18:41:40 +0700

    Add files via upload

---
+++ bai4.js
@@ -0,0 +1,5 @@
+let math = prompt("mời bạn nhập vào điểm toán");
+let physics = prompt("mời bạn nhập vào điểm vật lý");
+let chemistry = prompt("mời bạn nhập vào điểm hóa học");
+let total = (Number(math)+Number(physics)+Number(chemistry))/3;
+alert("điểm trung bình là"+total);
